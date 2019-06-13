f96dddf45b52b6688947b1ce9e901456204c8374
g3n3ralb3n-wp <51753752+g3n3ralb3n-wp@users.noreply.github.com>
2019-06-13 10:30:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
   description = 'small dictionary for spacy',   # Give a short description about your library
   author = 'Benjamin Johnson',                   # Type in your name
   author_email = 'benjaminallanjohnson@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  url = 'https://github.com/g3n3ralb3n-wp/en_core_web_sm_abd',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/g3n3ralb3n-wp/en_core_web_sm_abd/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'spacy',
